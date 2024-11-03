9ccb75edc0bf487d0210470517ac594ed9616214
nevinka2004 <yenuli.20232084@iit.ac.lk>
2024-11-03 22:58:28 +0530

    Added packages and configuration class

---
+++ src/main/java/com/ticketing_system/TicketingSystem/models/Configuration.java
@@ -0,0 +1,54 @@
+package com.ticketing_system.TicketingSystem.models;
+
+public class Configuration {
+    private int totalTickets;
+    private int ticketReleaseRate;
+    private int customerRetrievalRate;
+    private int maxTicketCapacity;
+
+    public Configuration() {
+    }
+
+    public Configuration(int ticketReleaseRate, int customerRetrievalRate, int maxTicketCapacity, int totalTickets) {
+        this.ticketReleaseRate = ticketReleaseRate;
+        this.customerRetrievalRate = customerRetrievalRate;
+        this.maxTicketCapacity = maxTicketCapacity;
+        this.totalTickets = totalTickets;
+    }
+
+
+    public int getMaxTicketCapacity() {
+        return maxTicketCapacity;
+    }
+
+    public void setMaxTicketCapacity(int maxTicketCapacity) {
+        this.maxTicketCapacity = maxTicketCapacity;
+    }
+
+    public int getCustomerRetrievalRate() {
+        return customerRetrievalRate;
+    }
+
+    public void setCustomerRetrievalRate(int customerRetrievalRate) {
+        this.customerRetrievalRate = customerRetrievalRate;
+    }
+
+    public int getTicketReleaseRate() {
+        return ticketReleaseRate;
+    }
+
+    public void setTicketReleaseRate(int ticketReleaseRate) {
+        this.ticketReleaseRate = ticketReleaseRate;
+    }
+
+    public int getTotalTickets() {
+        return totalTickets;
+    }
+
+    public void setTotalTickets(int totalTickets) {
+        this.totalTickets = totalTickets;
+    }
+
+
+}
+
